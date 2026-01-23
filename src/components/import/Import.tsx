d9077f39aa6271c0ccb02aaa4395d1e7fa4da269
Khakim Bakhramov <khakimbakhramov@gmail.com>
2026-01-23 10:12:22 +0500

    feat: edit

--- src/components/import/Import.tsx
+++ src/components/import/Import.tsx
@@ -2,6 +2,7 @@ import useLocalStorage from "use-local-storage";
 import { toast } from "react-toastify";
 import { useState, useRef } from "react";
 import * as XLSX from "xlsx";
+import { useNavigate } from "react-router-dom";
 import "./Import.css";
 
 type ExcelRow = Record<string, string | number | boolean | null>;
@@ -10,8 +11,10 @@ export const Import = () => {
   const [file, setFile] = useState<File | null>(null);
   const [, setRawData] = useLocalStorage("rawData", "");
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
     const selectedFile = e.target.files?.[0] || null;
     setFile(selectedFile);
   };
@@ -30,6 +33,7 @@ export const Import = () => {
     const reader = new FileReader();
 
     reader.onload = (event) => {
+      event.preventDefault();
       const buffer = event.target?.result;
       if (!buffer) {
         toast.error("Не верный контент файла");
@@ -64,7 +68,7 @@ export const Import = () => {
         onChange={handleFileChange}
         className="import-input"
       />
-      <button disabled={!file} className="import-button" onClick={handleImport}>
+      <button type="button" disabled={!file} className="import-button" onClick={handleImport}>
         Импортировать
       </button>
     </form>
