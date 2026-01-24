e0ac3754b695c58d6c0dbfe7936aa4d328c52974
Khakim Bakhramov <khakimbakhramov@gmail.com>
2026-01-24 18:42:34 +0500

    feat: table

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,35 +1,35 @@
-import { BarChartComponent } from "../chart/BarChartComponent.tsx";
-import useLocalStorage from "use-local-storage";
-import { sumData } from "../../utils/groupByField.tsx";
-import "./Product.css";
-
-type ProductPropsType = {
-  productName: string;
-  managerName: string;
-};
-
-export const Product = ({ productName, managerName }: ProductPropsType) => {
-  const [rawData] = useLocalStorage("rawData", "");
-  const sales = sumData(rawData ? JSON.parse(rawData) : [], "Sum", {
-    Manager: managerName,
-    Product: productName,
-  });
-  const akb = sumData(rawData ? JSON.parse(rawData) : [], "AKB", {
-    Manager: managerName,
-    Product: productName,
-  });
-
-  return (
-    <div className="w-100">
-      <p className="manager-header">{productName}</p>
-      <div>
-        <div className="manager-sales">
-          {/*<BarChartComponent aggregatedData={sales} height={90} />*/}
-        </div>
-        <div className="manager-akb">
-          {/*<BarChartComponent aggregatedData={akb} height={90} />*/}
-        </div>
-      </div>
-    </div>
-  );
-};
+// import { BarChartComponent } from "../chart/BarChartComponent.tsx";
+// import useLocalStorage from "use-local-storage";
+// import { sumData } from "../../utils/groupByField.tsx";
+// import "./Product.css";
+//
+// type ProductPropsType = {
+//   productName: string;
+//   managerName: string;
+// };
+//
+// export const Product = ({ productName, managerName }: ProductPropsType) => {
+//   const [rawData] = useLocalStorage("rawData", "");
+//   // const sales = sumData(rawData ? JSON.parse(rawData) : [], "Sum", {
+//   //   Manager: managerName,
+//   //   Product: productName,
+//   // });
+//   // const akb = sumData(rawData ? JSON.parse(rawData) : [], "AKB", {
+//   //   Manager: managerName,
+//   //   Product: productName,
+//   // });
+//
+//   return (
+//     <div className="w-100">
+//       <p className="manager-header">{productName}</p>
+//       <div>
+//         <div className="manager-sales">
+//           {/*<BarChartComponent aggregatedData={sales} height={90} />*/}
+//         </div>
+//         <div className="manager-akb">
+//           {/*<BarChartComponent aggregatedData={akb} height={90} />*/}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
