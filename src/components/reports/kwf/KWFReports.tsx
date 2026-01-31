a261b930108c70607b9ebb2267fa7cf11722cec3
Khakim Bakhramov <khakimbakhramov@gmail.com>
2026-01-31 19:17:07 +0500

    feat: table

--- src/components/reports/kwf/KWFReports.tsx
+++ src/components/reports/kwf/KWFReports.tsx
@@ -4,129 +4,141 @@ import "../Reports.css";
 
 export const KWFReports = () => {
   return (
-    <div className="d-flex">
-      <div className="sales">
-        <Title name="Сотув" />
-        <BarChartComponent type="Sum" title="ЖАМИ" />
-        <Title name="Продукт" />
-        <div className="d-flex gap-3">
-          <BarChartComponent productName="non-RSC" type="Sum" title="non-RSC" />
-          <BarChartComponent productName="wRSC" type="Sum" title="wRSC" />
-          <BarChartComponent productName="RSC" type="Sum" title="RSC" />
+    <>
+      <div className="d-flex align-items-center gap-3 px-3">
+        <div className="w-100">
+          <Title name="Сотув" />
+          <BarChartComponent type="Sum" title="ЖАМИ" />
         </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent productName="SnP" type="Sum" title="SnP" />
-          <BarChartComponent productName="SnP Lam" type="Sum" title="SnP Lam" />
-          <BarChartComponent
-            productName="Гофролист"
-            type="Sum"
-            title="Гофролист"
-          />
+        <div className="w-100">
+          <Title name="KPI" />
+          <BarChartComponent type="Sum" title="KPI" />
         </div>
-        <Title name="Регион" />
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Хоразм вилояти ва Коракалпогистон"
-            type="Sum"
-            title="Хоразм ва ККП"
-          />
-          <BarChartComponent
-            regionName="Бухоро ва Навоий вилояти"
-            type="Sum"
-            title="Бухоро ва Навоий"
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Самарканд, Жиззах ва Сирдайрё вилояти"
-            type="Sum"
-            title="Сам. Жиз. Сир."
-          />
-          <BarChartComponent
-            regionName="Тошкент вилояти ва шахри"
-            type="Sum"
-            title="Тошкент"
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Водий: ФАН"
-            type="Sum"
-            title="Водий: ФАН"
-          />
-          <BarChartComponent
-            regionName="Кашкадарё ва Сурхондарё вилояти"
-            type="Sum"
-            title="Каш. ва Сурх."
-          />
-          <BarChartComponent
-            regionName="Экспорт (кушни)"
-            type="Sum"
-            title="Экспорт к."
-          />
+        <div className="w-100">
+          <Title name="АКБ" />
+          <BarChartComponent type="AKB" title="ЖАМИ" />
         </div>
       </div>
-      <div className="akb">
-        <Title name="АКБ" />
-        <BarChartComponent type="AKB" title="ЖАМИ" />
-        <Title name="Продукт" />
-        <div className="d-flex gap-3">
-          <BarChartComponent productName="wRSC" type="AKB" title="wRSC" />
-          <BarChartComponent productName="non-RSC" type="AKB" title="non-RSC" />
-          <BarChartComponent productName="RSC" type="AKB" title="RSC" />
+      <div className="d-flex">
+        <div className="sales">
+          <Title name="Продукт" />
+          <div className="d-flex gap-3">
+            <BarChartComponent productName="non-RSC" type="Sum" title="non-RSC" />
+            <BarChartComponent productName="wRSC" type="Sum" title="wRSC" />
+            <BarChartComponent productName="RSC" type="Sum" title="RSC" />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent productName="SnP" type="Sum" title="SnP" />
+            <BarChartComponent productName="SnP Lam" type="Sum" title="SnP Lam" />
+            <BarChartComponent
+                productName="Гофролист"
+                type="Sum"
+                title="Гофролист"
+            />
+          </div>
+          <Title name="Регион" />
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Хоразм вилояти ва Коракалпогистон"
+                type="Sum"
+                title="Хоразм ва ККП"
+            />
+            <BarChartComponent
+                regionName="Бухоро ва Навоий вилояти"
+                type="Sum"
+                title="Бухоро ва Навоий"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Самарканд, Жиззах ва Сирдайрё вилояти"
+                type="Sum"
+                title="Сам. Жиз. Сир."
+            />
+            <BarChartComponent
+                regionName="Тошкент вилояти ва шахри"
+                type="Sum"
+                title="Тошкент"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Водий: ФАН"
+                type="Sum"
+                title="Водий: ФАН"
+            />
+            <BarChartComponent
+                regionName="Кашкадарё ва Сурхондарё вилояти"
+                type="Sum"
+                title="Каш. ва Сурх."
+            />
+            <BarChartComponent
+                regionName="Экспорт (кушни)"
+                type="Sum"
+                title="Экспорт к."
+            />
+          </div>
         </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent productName="SnP" type="AKB" title="SnP" />
-          <BarChartComponent productName="SnP Lam" type="AKB" title="SnP Lam" />
-          <BarChartComponent
-            productName="Гофролист"
-            type="AKB"
-            title="Гофролист"
-          />
-        </div>
-        <Title name="Регион" />
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Хоразм вилояти ва Коракалпогистон"
-            type="AKB"
-            title="Хоразм ва ККП"
-          />
-          <BarChartComponent
-            regionName="Бухоро ва Навоий вилояти"
-            type="AKB"
-            title="Бухоро ва Навоий"
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Самарканд, Жиззах ва Сирдайрё вилояти"
-            type="AKB"
-            title="Сам. Жиз. Сир."
-          />
-          <BarChartComponent
-            regionName="Тошкент вилояти ва шахри"
-            type="AKB"
-            title="Тошкент"
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Водий: ФАН"
-            type="AKB"
-            title="Водий: ФАН"
-          />
-          <BarChartComponent
-            regionName="Кашкадарё ва Сурхондарё вилояти"
-            type="AKB"
-            title="Каш. ва Сурх."
-          />
-          <BarChartComponent
-            regionName="Экспорт (кушни)"
-            type="AKB"
-            title="Экспорт к."
-          />
+        <div className="akb">
+          <Title name="Продукт" />
+          <div className="d-flex gap-3">
+            <BarChartComponent productName="wRSC" type="AKB" title="wRSC" />
+            <BarChartComponent productName="non-RSC" type="AKB" title="non-RSC" />
+            <BarChartComponent productName="RSC" type="AKB" title="RSC" />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent productName="SnP" type="AKB" title="SnP" />
+            <BarChartComponent productName="SnP Lam" type="AKB" title="SnP Lam" />
+            <BarChartComponent
+                productName="Гофролист"
+                type="AKB"
+                title="Гофролист"
+            />
+          </div>
+          <Title name="Регион" />
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Хоразм вилояти ва Коракалпогистон"
+                type="AKB"
+                title="Хоразм ва ККП"
+            />
+            <BarChartComponent
+                regionName="Бухоро ва Навоий вилояти"
+                type="AKB"
+                title="Бухоро ва Навоий"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Самарканд, Жиззах ва Сирдайрё вилояти"
+                type="AKB"
+                title="Сам. Жиз. Сир."
+            />
+            <BarChartComponent
+                regionName="Тошкент вилояти ва шахри"
+                type="AKB"
+                title="Тошкент"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Водий: ФАН"
+                type="AKB"
+                title="Водий: ФАН"
+            />
+            <BarChartComponent
+                regionName="Кашкадарё ва Сурхондарё вилояти"
+                type="AKB"
+                title="Каш. ва Сурх."
+            />
+            <BarChartComponent
+                regionName="Экспорт (кушни)"
+                type="AKB"
+                title="Экспорт к."
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
--- src/components/reports/mp/MPReports.tsx
+++ src/components/reports/mp/MPReports.tsx
@@ -4,187 +4,199 @@ import { Title } from "../../title/Title.tsx";
 
 export const MPReports = () => {
   return (
-    <div className="d-flex">
-      <div className="sales">
-        <Title name="Сотув" />
-        <BarChartComponent type="Sum" title="ЖАМИ" />
-        <Title name="Продукт" />
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            productName="Karton CelLayner"
-            type="Sum"
-            title="CelLa"
-          />
-          <BarChartComponent
-            productName="Karton OqLayner"
-            type="Sum"
-            title="OqLa"
-          />
+    <>
+      <div className="d-flex align-items-center gap-3 px-3">
+        <div className="w-100">
+          <Title name="Сотув" />
+          <BarChartComponent type="Sum" title="ЖАМИ" />
         </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            productName="Karton К0 120гр"
-            type="Sum"
-            title="К0"
-          />
-          <BarChartComponent
-            productName="Karton К1 120гр"
-            type="Sum"
-            title="К1"
-          />
+        <div className="w-100">
+          <Title name="KPI" />
+          <BarChartComponent type="Sum" title="KPI" />
         </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent productName="Karton B1" type="Sum" title="B1" />
-          <BarChartComponent
-            productName="Karton B2+ 100gr"
-            type="Sum"
-            title="B2+ 100gr"
-          />
-          <BarChartComponent
-            productName="Karton B2+ 120gr"
-            type="Sum"
-            title="B2+ 120gr"
-          />
-        </div>
-        <Title name="Регион" />
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Хоразм ва Коракалпогистон"
-            type="Sum"
-            title="Хоразм ва ККП"
-          />
-          <BarChartComponent
-            regionName="Бухоро ва Навоий"
-            type="Sum"
-            title="Бухоро ва Навоий"
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Водий: (ФАН)"
-            type="Sum"
-            title="Водий: (ФАН)"
-          />
-          <BarChartComponent
-            regionName="Сам, Джиз, Сирд"
-            type="Sum"
-            title="Сам, Джиз, Сирд"
-          />
-          <BarChartComponent
-            regionName="Тошкент вилоят ва шахар"
-            type="Sum"
-            title="Тошкент"
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Кашкадарё ва Сурхондарё"
-            type="Sum"
-            title="Каш. ва Сурх."
-          />
-          <BarChartComponent
-            regionName="Eksport (кушни)"
-            type="Sum"
-            title="Экспорт к."
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Karton Works"
-            type="Sum"
-            title="Karton Works"
-          />
-          <BarChartComponent
-            regionName="Дилер: Union Paper"
-            type="Sum"
-            title="Диллер"
-          />
+        <div className="w-100">
+          <Title name="АКБ" />
+          <BarChartComponent type="AKB" title="ЖАМИ" />
         </div>
       </div>
-      <div className="akb">
-        <Title name="АКБ" />
-        <BarChartComponent type="AKB" title="ЖАМИ" />
-        <Title name="Продукт" />
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            productName="Karton CelLayner"
-            type="AKB"
-            title="CelLa"
-          />
-          <BarChartComponent
-            productName="Karton OqLayner"
-            type="AKB"
-            title="OqLa"
-          />
+      <div className="d-flex">
+        <div className="sales">
+          <Title name="Продукт" />
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                productName="Karton CelLayner"
+                type="Sum"
+                title="CelLa"
+            />
+            <BarChartComponent
+                productName="Karton OqLayner"
+                type="Sum"
+                title="OqLa"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                productName="Karton К0 120гр"
+                type="Sum"
+                title="К0"
+            />
+            <BarChartComponent
+                productName="Karton К1 120гр"
+                type="Sum"
+                title="К1"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent productName="Karton B1" type="Sum" title="B1" />
+            <BarChartComponent
+                productName="Karton B2+ 100gr"
+                type="Sum"
+                title="B2+ 100gr"
+            />
+            <BarChartComponent
+                productName="Karton B2+ 120gr"
+                type="Sum"
+                title="B2+ 120gr"
+            />
+          </div>
+          <Title name="Регион" />
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Хоразм ва Коракалпогистон"
+                type="Sum"
+                title="Хоразм ва ККП"
+            />
+            <BarChartComponent
+                regionName="Бухоро ва Навоий"
+                type="Sum"
+                title="Бухоро ва Навоий"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Водий: (ФАН)"
+                type="Sum"
+                title="Водий: (ФАН)"
+            />
+            <BarChartComponent
+                regionName="Сам, Джиз, Сирд"
+                type="Sum"
+                title="Сам, Джиз, Сирд"
+            />
+            <BarChartComponent
+                regionName="Тошкент вилоят ва шахар"
+                type="Sum"
+                title="Тошкент"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Кашкадарё ва Сурхондарё"
+                type="Sum"
+                title="Каш. ва Сурх."
+            />
+            <BarChartComponent
+                regionName="Eksport (кушни)"
+                type="Sum"
+                title="Экспорт к."
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Karton Works"
+                type="Sum"
+                title="Karton Works"
+            />
+            <BarChartComponent
+                regionName="Дилер: Union Paper"
+                type="Sum"
+                title="Диллер"
+            />
+          </div>
         </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            productName="Karton К0 120гр"
-            type="AKB"
-            title="К0"
-          />
-          <BarChartComponent
-            productName="Karton К1 120гр"
-            type="AKB"
-            title="К1"
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent productName="Karton B1" type="AKB" title="B1" />
-          <BarChartComponent
-            productName="Karton B2+ 100gr"
-            type="AKB"
-            title="B2+ 100gr"
-          />
-          <BarChartComponent
-            productName="Karton B2+ 120gr"
-            type="AKB"
-            title="B2+ 120gr"
-          />
-        </div>
-        <Title name="Регион" />
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Хоразм ва Коракалпогистон"
-            type="AKB"
-            title="Хоразм ва ККП"
-          />
-          <BarChartComponent
-            regionName="Бухоро ва Навоий"
-            type="AKB"
-            title="Бухоро ва Навоий"
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Водий: (ФАН)"
-            type="AKB"
-            title="Водий: (ФАН)"
-          />
-          <BarChartComponent
-            regionName="Сам, Джиз, Сирд"
-            type="AKB"
-            title="Сам, Джиз, Сирд"
-          />
-          <BarChartComponent
-            regionName="Тошкент вилоят ва шахар"
-            type="AKB"
-            title="Тошкент"
-          />
-        </div>
-        <div className="d-flex gap-3">
-          <BarChartComponent
-            regionName="Кашкадарё ва Сурхондарё"
-            type="AKB"
-            title="Каш. ва Сурх."
-          />
-          <BarChartComponent
-            regionName="Eksport (кушни)"
-            type="AKB"
-            title="Экспорт к."
-          />
+        <div className="akb">
+          <Title name="Продукт" />
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                productName="Karton CelLayner"
+                type="AKB"
+                title="CelLa"
+            />
+            <BarChartComponent
+                productName="Karton OqLayner"
+                type="AKB"
+                title="OqLa"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                productName="Karton К0 120гр"
+                type="AKB"
+                title="К0"
+            />
+            <BarChartComponent
+                productName="Karton К1 120гр"
+                type="AKB"
+                title="К1"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent productName="Karton B1" type="AKB" title="B1" />
+            <BarChartComponent
+                productName="Karton B2+ 100gr"
+                type="AKB"
+                title="B2+ 100gr"
+            />
+            <BarChartComponent
+                productName="Karton B2+ 120gr"
+                type="AKB"
+                title="B2+ 120gr"
+            />
+          </div>
+          <Title name="Регион" />
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Хоразм ва Коракалпогистон"
+                type="AKB"
+                title="Хоразм ва ККП"
+            />
+            <BarChartComponent
+                regionName="Бухоро ва Навоий"
+                type="AKB"
+                title="Бухоро ва Навоий"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Водий: (ФАН)"
+                type="AKB"
+                title="Водий: (ФАН)"
+            />
+            <BarChartComponent
+                regionName="Сам, Джиз, Сирд"
+                type="AKB"
+                title="Сам, Джиз, Сирд"
+            />
+            <BarChartComponent
+                regionName="Тошкент вилоят ва шахар"
+                type="AKB"
+                title="Тошкент"
+            />
+          </div>
+          <div className="d-flex gap-3">
+            <BarChartComponent
+                regionName="Кашкадарё ва Сурхондарё"
+                type="AKB"
+                title="Каш. ва Сурх."
+            />
+            <BarChartComponent
+                regionName="Eksport (кушни)"
+                type="AKB"
+                title="Экспорт к."
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
